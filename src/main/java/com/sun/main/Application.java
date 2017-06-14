078384f8871784a83dd608d28b8d5940c54d7a59
sun0110 <balancebug@163.com>
2017-06-14 10:17:49 +0800

    dd

--- src/main/java/com/sun/main/Application.java
+++ src/main/java/com/sun/main/Application.java
@@ -10,7 +10,7 @@ public class Application implements EmbeddedServletContainerCustomizer{
 
 	@Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(80);
+		container.setPort(8082);
 	}
 	
 	public static void main(String[] args) {
